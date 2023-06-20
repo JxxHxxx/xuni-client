e43d88464aa06fe87e0154b1f2a68946155f94f5
JxxHxxx <leesin5498@naver.com>
2023-06-21 01:58:52 +0900

    Refactor/스타일과 컴포넌트 분리 (#1)

--- src/components/GroupList.js
+++ src/components/GroupList.js
@@ -1,30 +1,6 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { styled } from 'styled-components';
-
-const GroupLayOut = styled.div`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr); /* 4개의 열을 생성합니다. */
-    grid-gap: 16px; /* 아이템 사이의 간격을 설정합니다. */
-`;
-
-const GroupItem = styled.div`
-    flex: 0 0 calc(50% - 8px); /* 아이템의 너비 설정 (반 너비로 배치, 간격 제외) */
-    margin: 4px; /* 아이템 간격 설정 */
-
-    width: 400px;
-    height: 180px;
-    border: 1px solid black;
-    box-shadow: 5px 5px 20px;
-    margin: auto;
-    margin-bottom: 30px;
-    transition: all 0.5s;
-    transition-delay: 0.4s;
-`
-
-const Title = styled.h1`
-  grid-column: 1 / -1;
-`
+import * as GroupStyle from '../style/GroupStyle';
 
 const GroupList = (className) => {
   const [groups, setGroups] = useState([]);
@@ -48,10 +24,10 @@ const GroupList = (className) => {
   }
 
   return (
-    <GroupLayOut>
-      <Title>스터디 그룹</Title>
+    <GroupStyle.LayOut>
+      <GroupStyle.Title>스터디 그룹</GroupStyle.Title>
       {groups.map(group => (
-        <GroupItem key={group.groupId}>
+        <GroupStyle.GroupItem key={group.groupId}>
           <div>Group ID : {group.groupId}</div>
           <div>Group Status : {group.groupStatus}</div>
           <div>주최자 : {group.host.hostName}</div>
@@ -59,9 +35,9 @@ const GroupList = (className) => {
           <div>시작일 : {group.period.startDate} 종료일 : {group.period.endDate}</div>
           <div>To {group.time.startTime} ~ {group.time.endTime}</div>
           <div>현재 남은 스터디 좌석 : {group.capacity.leftCapacity}</div>
-        </GroupItem>
+        </GroupStyle.GroupItem>
       ))}
-    </GroupLayOut>
+    </GroupStyle.LayOut>
   );
 };
 
--- src/components/StudyProductList.js
+++ src/components/StudyProductList.js
@@ -1,30 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { styled } from 'styled-components';
-
-const StudyProductLayOut = styled.div`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr); /* 4개의 열을 생성합니다. */
-    grid-gap: 16px; /* 아이템 사이의 간격을 설정합니다. */
-`
-const StudyProductItem = styled.div`
-    flex: 0 0 calc(50% - 8px); /* 아이템의 너비 설정 (반 너비로 배치, 간격 제외) */
-    margin: 4px; /* 아이템 간격 설정 */
-
-    width: 400px;
-    height: 150px;
-    border: 1px solid black;
-    box-shadow: 5px 5px 20px;
-    margin: auto;
-    margin-bottom: 30px;
-    transition: all 0.5s;
-    transition-delay: 0.4s;
-`
-
-const Title = styled.h1`
-    margin: 0;
-    grid-column: 1 / -1;
-`
+import * as StudyProductStyle from '../style/StudyProductStyle';
 
 const StudyProductList = () => {
     const [studyProducts, setStudyProducts] = useState([]);
@@ -46,17 +22,17 @@ const StudyProductList = () => {
     }, [])
 
     return (
-        <StudyProductLayOut>
-            <Title>스터디 상품 목록</Title>
+        <StudyProductStyle.LayOut>
+            <StudyProductStyle.Title>스터디 상품 목록</StudyProductStyle.Title>
             {studyProducts.map(studyProduct => (
-                <StudyProductItem key={studyProduct.studyProductId} className="study-product-list-item">
+                <StudyProductStyle.StudyProductItem key={studyProduct.studyProductId} className="study-product-list-item">
                     <div>studyProduct ID: {studyProduct.studyProductId}</div>
                     <div>Name: {studyProduct.name}</div>
                     <div>Category: {studyProduct.category}</div>
                     <div>Creator: {studyProduct.creator}</div>
-                </StudyProductItem>
+                </StudyProductStyle.StudyProductItem>
             ))}
-        </StudyProductLayOut>
+        </StudyProductStyle.LayOut>
     );
 };
 
